a2a506ccefb2efb468783fda582c20dc708026d9
My Name <passivevoice3@gmail.com>
2021-05-31 22:26:29 +0300

    add header and json base

--- src/index.js
+++ src/index.js
@@ -1,55 +1,69 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { Image, Header, Button } from 'semantic-ui-react'
-import 'semantic-ui-css/semantic.min.css';
+import {Button, Image, Navbar, Container, Nav } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+const bd = require('./public/bd/date.json')
+
+function EatList() {
+    const review = bd.map((item, index) => 
+            <div className='burger__preview'>
+            <Image height="277" width="277" src={item.path} alt={item.path}/>
+            <div>{item.name}</div>                
+            <div>{item.description}</div>                
+            <div>{item.price}</div>                
+            <Button variant="danger">Заказать</Button>
+        </div>
+    )
+
+    return (
+        <div className=''>
+            {review}
+        </div>
+    )
+}
 
 function HeaderMenu() {
     return (
         <div className="header">
             <div className="header__title">
-                <div className="header__title_logo" size> 
-                    <Image src="/src/public/img/burger_icon.jpg" size='tiny'  />
+                    <Image src="/src/public/img/burger_icon.jpg" height="70px"  width="70px"/>
                     <div>
-                        <Header as='h2'>♂️Right♂️ Burgers</Header>
-                        <Header as='h4'>Бургеры прямиком из Dungeon! </Header>
+                        <h2 className="header__title_h">♂️Right♂️ Burgers</h2>
+                        <h4 className="header__title_p">Бургеры прямиком из Dungeon! </h4>
                     </div>
-                </div>
-                <span>Доставка бэргеров</span>
-                    <a> Юпитер</a>
-                    <div>
-                        <p>8 800 228-13-37</p>
-                        <p>Звонок платный</p>
-                    </div>
+               
             </div>
             <div className="navigation">
-                <ul>
-                    <li>Гамбургеры</li>
-                    <li>Закуски</li>
-                    <li>Десерты</li>
-                    <li>Напитки</li>
-                    <li>Контакты</li>
-                    <div>
-                        <Button>Click Here</Button>
-                    </div>
-                </ul>
-
+                <Navbar expand="lg">
+                        <Navbar.Brand href="#home">Гамбургеры</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#link">Закуски</Nav.Link>
+                            <Nav.Link href="#link">Десерты</Nav.Link>
+                            <Nav.Link href="#link">Напитки</Nav.Link>
+                            <Nav.Link href="#link">Контакты</Nav.Link>
+                        </Nav>
+                        </Navbar.Collapse>
+                        <Button className="nav_btn" variant="success">Корзина</Button>
+                </Navbar>
             </div>
         </div>
-
     )
 }
 
 function Main() {
     return (
-        <div>
-         <Image src='/src/public/pic.jpg'  size='small' />
+        <div className="wrapper">
+            <HeaderMenu/>
+            <EatList/>
         </div>
     );
 }
 
 ReactDOM.render(
-    <HeaderMenu />,
+    <Main />,
     document.getElementById('root')
   );
 

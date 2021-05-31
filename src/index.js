6d55e2bc3d6ccd411283f53c945d6801f44f653e
bekanoor <passivevoice3@gmail.com>
2021-05-31 15:00:23 +0300

    add some layout

--- src/index.js
+++ src/index.js
@@ -1,20 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { Image } from 'semantic-ui-react'
+import { Image, Header, Button } from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css';
+
+function HeaderMenu() {
+    return (
+        <div className="header">
+            <div className="header__title">
+                <div className="header__title_logo" size> 
+                    <Image src="/src/public/img/burger_icon.jpg" size='tiny'  />
+                    <div>
+                        <Header as='h2'>♂️Right♂️ Burgers</Header>
+                        <Header as='h4'>Бургеры прямиком из Dungeon! </Header>
+                    </div>
+                </div>
+                <span>Доставка бэргеров</span>
+                    <a> Юпитер</a>
+                    <div>
+                        <p>8 800 228-13-37</p>
+                        <p>Звонок платный</p>
+                    </div>
+            </div>
+            <div className="navigation">
+                <ul>
+                    <li>Гамбургеры</li>
+                    <li>Закуски</li>
+                    <li>Десерты</li>
+                    <li>Напитки</li>
+                    <li>Контакты</li>
+                    <div>
+                        <Button>Click Here</Button>
+                    </div>
+                </ul>
+
+            </div>
+        </div>
+
+    )
+}
 
 function Main() {
     return (
         <div>
          <Image src='/src/public/pic.jpg'  size='small' />
-         <img src='/src/public/pic.jpg' />
         </div>
     );
 }
 
 ReactDOM.render(
-    <Main />,
+    <HeaderMenu />,
     document.getElementById('root')
   );
 
